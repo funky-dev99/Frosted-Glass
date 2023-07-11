b34d773369c7933402390642ed54ea6081db6914
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-07-11 22:31:22 +0530

    Update

---
+++ frosted_glass/lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: Image.asset("images/back.png").image, fit: BoxFit.cover),
+        ),
+
+      ),
+    );
+  }
+}
